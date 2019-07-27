5a0a45495edef6e6101beffe809f22ef200c95b2
Hafize Güngör <33956266+gungorhafize@users.noreply.github.com>
2019-07-27 05:46:56 +0300

    simple asteriks

---
+++ yildiz.java
@@ -0,0 +1,27 @@
+
+public class yildiz {
+
+	public static void main(String[] args) {
+
+		int i, j;
+		for(i=0; i<5;i++)
+		{
+			for(j=0;j<i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+		
+		for(i=5; i>0;i--)
+		{
+			for(j=0;j<i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.print("\n");
+
+		}
+	}
+
+}
